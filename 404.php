aedb8a30fd8393c6e1ee65c9316fc6a3d7761c16
tim booher <tim@theboohers.org>
2012-10-07 09:12:33 -0400

    updated layout

--- 404.php
+++ 404.php
@@ -9,39 +9,45 @@
 
 get_header(); ?>
 
-	<div id="primary">
-		<div id="content" role="main">
+<div class="LayoutGrid LayoutCenter ArticleGrid LayoutWideGrid LayoutP">
+    <div class="LayoutRow Link">
+        <div class="LayoutBreakAfter">
+                <header class="page-header">
+                    <h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentyeleven' ); ?></h1>
+                </header>
+                <div class="ArticleGrid-1 LayoutWideGrid-1 TextSmall PrintLayoutHide">
+                  &nbsp;
+                </div>
+                <div class="ArticleGrid-2-4" id="main">
+                    <div class="Column mnemonic photoshop-color">
+                        <article id="post-0" class="post error404 not-found">
 
-			<article id="post-0" class="post error404 not-found">
-				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentyeleven' ); ?></h1>
-				</header>
+                            <div class="entry-content">
+                                <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'twentyeleven' ); ?></p>
 
-				<div class="entry-content">
-					<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'twentyeleven' ); ?></p>
+                                <?php get_search_form(); ?>
 
-					<?php get_search_form(); ?>
+                                <?php the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ), array( 'widget_id' => '404' ) ); ?>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ), array( 'widget_id' => '404' ) ); ?>
+                                <div class="widget">
+                                    <h2 class="widgettitle"><?php _e( 'Most Used Categories', 'twentyeleven' ); ?></h2>
+                                    <ul>
+                                    <?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
+                                    </ul>
+                                </div>
 
-					<div class="widget">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'twentyeleven' ); ?></h2>
-						<ul>
-						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
-						</ul>
-					</div>
+                                <?php
+                                /* translators: %1$s: smilie */
+                                $archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'twentyeleven' ), convert_smilies( ':)' ) ) . '</p>';
+                                the_widget( 'WP_Widget_Archives', array('count' => 0 , 'dropdown' => 1 ), array( 'after_title' => '</h2>'.$archive_content ) );
+                                ?>
 
-					<?php
-					/* translators: %1$s: smilie */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'twentyeleven' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', array('count' => 0 , 'dropdown' => 1 ), array( 'after_title' => '</h2>'.$archive_content ) );
-					?>
+                                <?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-				</div><!-- .entry-content -->
-			</article><!-- #post-0 -->
-
+                            </div><!-- .entry-content -->
+                        </article><!-- #post-0 -->
+                    </div>
+                </div>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
