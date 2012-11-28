e38df11c0b4180ff5c6e9a55822ca7d289a707fd
Tim Booher <tim@theboohers.org>
2012-11-27 22:19:39 -0500

    updated nivo order

--- index.php
+++ index.php
@@ -23,13 +23,13 @@
                           <img src="/wp-content/themes/crossfit_fallschurch_theme/images/front_page/community2.jpg" alt="" >
                         </a>
                         <a href="/get-started/">
-                          <img src="/wp-content/themes/crossfit_fallschurch_theme/images/front_page/free-workout2.jpg" alt="" >
+                          <img src="/wp-content/themes/crossfit_fallschurch_theme/images/front_page/train2.jpg" alt="" >
                         </a>
                         <a href="/get-started/">
                           <img src="/wp-content/themes/crossfit_fallschurch_theme/images/front_page/results2.jpg" alt="" >
                         </a>
                         <a href="/get-started/">
-                          <img src="/wp-content/themes/crossfit_fallschurch_theme/images/front_page/train2.jpg" alt="" >
+                          <img src="/wp-content/themes/crossfit_fallschurch_theme/images/front_page/free-workout2.jpg" alt="" >
                         </a>
                     </div>
                 </div>
@@ -41,9 +41,9 @@
                         jQuery('#slider').nivoSlider({
                          effect: 'fade',
                          randomStart: false,
-                         animSpeed: 4000,
                          pauseOnHover: true
                          });
+                         // animSpeed: 4000
                     });
                 </script>
             </div>
