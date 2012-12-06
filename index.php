9f6647abbc48ebfa919491fdf14315ca6073881b
Tim Booher <tim@theboohers.org>
2012-12-06 23:14:12 +0100

    updated

--- index.php
+++ index.php
@@ -84,12 +84,12 @@
             <div class="ArticleGrid-4">
                 <div id="infobar">
                     <h2 style="font-size:2em;">Why CFFC?</h2>
-                    <p style="text-align:center; padding-bottom:12px"><a href="https://crossfitfallschurch.zenplanner.com/zenplanner/portal/sign-up-now.cfm">Sign Up Now</a></p>
                     <ul class="dash-list">
                        <li>Supportive community</li>
                        <li>Professional and caring coaches</li>
                        <li>Commitment to your success</li>
                     </ul>
+                    <p style="padding-bottom:12px"><a href="https://crossfitfallschurch.zenplanner.com/zenplanner/portal/sign-up-now.cfm">Sign Up Now</a></p>
                     <h2>News</h2>
                     <p>
                         Be on the lookout for details about our grand opening.
